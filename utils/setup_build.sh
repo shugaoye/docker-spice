cc63fc601a0b30159f39ee86b60ee188ea49e405
Roger Ye <shugaoye@yahoo.com>
2017-09-05 16:29:04 +0800

    Updated setup script.

--- utils/setup_build.sh
+++ utils/setup_build.sh
@@ -9,3 +9,4 @@ if [ ! -e $INST_ROOT ]; then mkdir -p $INST_ROOT; fi
 
 export PKG_CONFIG_PATH=$INST_ROOT/lib/pkgconfig:$INST_ROOT/share/pkgconfig
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INST_ROOT/lib
+export PATH=$PATH:$INST_ROOT/bin
